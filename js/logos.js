8b0d87cb2a18fb72122ec0644684ef62ee17c4ed
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-02-25 20:52:56 -0300

    Add files via upload

---
+++ js/logos.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+              $('#marcas').owlCarousel({
+                items: 4,
+                 autoplay: true,
+                lazyLoad: true,
+                nav:false,
+                loop: true,
+                margin: 15,
+              });
+            });
---
+++ js/vitrine.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+              loop:true,
+              margin:50,
+              nav:true,
+              responsive:{
+              0:{
+                  items:1
+              },
+              1000:{
+                  items:3
+              },
+              2000:{
+                items:5
+              }
+    }
+})
+
