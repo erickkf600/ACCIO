0200c001512d0d24f77354def786a0f4338efc87
erickkf600 <35529628+erickkf600@users.noreply.github.com>
2018-03-07 16:12:46 -0300

    Update login.php

--- login.php
+++ login.php
@@ -9,11 +9,11 @@
         <div class="modal-body">
            <div class="form-group">
                 <label for="exampleInputEmail1">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                <input required type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Senha</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="senha">
+                <input required type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="senha">
             </div>
               <button type="submit" class="btn btn-dark btn-block">Login</button>
               <label>
